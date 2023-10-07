be3a972c4e50ee23b074564e722c45c10b32a508
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-07 22:11:43 +0600

    commit

---
+++ 02_data_types.js
@@ -0,0 +1,14 @@
+// primitive data types - 7
+// nn bb ss u
+// null, number
+// boolean, BigInt,
+// string, symbol
+// undefined
+let a = null; // null
+let b = 345; // number
+let c = true; // boolean
+let d = BigInt("567"); // BigInt
+let e = "Sadiqur Rahman"; // string
+let f = Symbol("I am a nice symbol"); // symbol
+let g ; // undefinedd
+console.log(a,b,c,d,e,f,g,);
