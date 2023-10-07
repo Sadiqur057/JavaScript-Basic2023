d2779c8d9d14d31945c79121202a1c7d4975f4a8
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-07 19:51:20 +0600

    intro variables

---
+++ 01_var.js
@@ -0,0 +1,19 @@
+// Variable => A container that stores value, A symbolic name for a value or data storage location that can change during the execution of a program
+
+// JS is Dynamically Typed Language => We can change variable type in runtime
+
+// Declare a variable
+
+var a = 67; // old
+let b= 24; // new
+// a,b => identifier
+// = is a assignment operator
+// 67,24 is value;
+let full_name = "Sadiqur Rahman";
+let department = "CSE";
+let age;
+const pi = 3.1416;
+age= 23;
+age=24;
+// we can change value of variable except const variable.
+console.log(a,b,full_name,department,age,pi);
