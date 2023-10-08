294df11e301e0799178d903e1ce724c0f5c02ef5
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-08 10:10:15 +0600

    commit

---
+++ 03_qus.js
@@ -0,0 +1,9 @@
+// qus 1- Create a variable with string data type and add a nummber with it
+
+// qus 2- Use typeof operator to find the datatype of the string variable from the last qus.
+
+// qus 3- Create a const object in javascript. Can you change it later?
+
+// qus 4- Try to add a new key to const object in qus 3, is it possible?
+
+// qus 5- write a program in js to create a word meaning of dictionary of 5 words
