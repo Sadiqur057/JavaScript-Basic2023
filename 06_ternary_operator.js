4a674b4bf593fcfef5d7e300d39323d84b8dca5a
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-09 23:13:34 +0600

    commit

---
+++ 06_ternary_operator.js
@@ -0,0 +1,4 @@
+let a = 5;
+let b = 7;
+// (condition?if true: if)
+console.log(a>b?"a is greater than b":"b is greated than a");
---
+++ 07_types_of_loop.js
@@ -0,0 +1,5 @@
+// for loop, while loop, do while loop => repeated operation based on given condition
+
+// for in loop => used to loop in keys of object
+
+// for of loop => used to loop in through values of the object
