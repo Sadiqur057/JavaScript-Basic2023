bd2a7ad15f6dc2c95308e757eff34055c7abcc66
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-09 22:11:02 +0600

    commit

---
+++ 06_conditional_statements.js
@@ -0,0 +1,13 @@
+// if else
+// if elseif else
+
+
+let age = prompt("Ener your age: ");
+
+if(age>0){
+    alert("not a valid age.");
+}else if(age<18){
+    alert("You are under age..");
+}else{
+    alert("Welcome to the Jungle safary.");
+}
