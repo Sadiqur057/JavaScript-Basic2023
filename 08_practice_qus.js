0de23f3943439ae083d9cd97d82dabfd76a9a875
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 01:46:48 +0600

    commit

---
+++ 08_practice_qus.js
@@ -0,0 +1,8 @@
+// qus 1 => Write a program to print the marks of a student in an object using for loop
+// stud = { sadiq : 98 , mahin : 95}
+
+// qus 2 => Write the program in qus 1 using for in loop
+
+// qus 3 => write a program to find "try again" until the user enters the correct number
+
+// qus 4 => write a function to find mean of 5 number
