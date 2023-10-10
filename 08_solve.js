8339739da88cd57de92e44e162a55cbd322b1353
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 01:50:13 +0600

    commit

---
+++ 08_solve.js
@@ -0,0 +1,12 @@
+// qus 1 => Write a program to print the marks of a student in an object using for loop
+
+let students = { sadiq : 98 , mahin : 95 , hridoy : 87 , hemel : 97 };
+for(stud in students){
+    console.log(`Marks of ${stud} is ${students[stud]}`);
+}
+
+// qus 2 => Write the program in qus 1 using for in loop
+
+// qus 3 => write a program to find "try again" until the user enters the correct number
+
+// qus 4 => write a function to find mean of 5 number
