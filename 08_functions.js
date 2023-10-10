b8c23bce4c8fae552a95222acdaef6bac3a8e0ac
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 01:41:17 +0600

    commit

--- 08_functions.js
+++ 08_functions.js
@@ -1,6 +1,26 @@
 // Function is a block of code designed to perform a particular task
 
-function addTwoNum(a,b){
-    return a+b;
+const hello =()=>{
+    console.log("Hello there!");
 }
-console.log(addTwoNum(2,3));
+hello();
+let x=7;
+let y=9;
+let z=6;
+
+function getSum(a,b,c){
+    console.log( `Sum of the numbers is ${a+b+c}`);
+}
+getSum(x,y,z);
+
+function getAvg(a,b,c){
+    console.log( `Averagge of the numbers is ${(a+b+c)/3}`);
+}
+getAvg(x,y,z);
+
+
+const getMul = (a,b,c)=>{
+    return a*b*c;
+}
+console.log(`Multiplication of number is ${getMul(x,y,z)}`);
+
