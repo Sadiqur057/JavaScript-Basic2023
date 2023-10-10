3c8d066339a360bdb35e1d12467fa2eb82111c4a
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-10 15:15:46 +0600

    commit

---
+++ 06_solve.js
@@ -0,0 +1,9 @@
+// qus 1 => Use logical operator to find whether the age of a person lies between 10 and 20?
+
+// qus 2 => Demonstrate the use of switch case statements in javascript
+
+// qus 3 => Write a javascript program to find whether a number is divisible by 2 and 3
+
+// qus 4 => Write a javascript program to find whether a number  is divisible by either 2 or 3
+
+// qus 5 => Print "You can Drive" or "You cannot Drive" Based on age being greater than 18 using ternary operator.
