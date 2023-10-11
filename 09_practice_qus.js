aa1d7f0812aaef321874da66d463e9ecb52ca928
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 19:23:06 +0600

    commit

---
+++ 09_practice_qus.js
@@ -0,0 +1,10 @@
+// qus 1 => What will the following print in Javascript
+    // console.log("har\"".length);
+
+// qus 2 => Explore the includes. starts with & end with function of a string
+
+// qus 3 => Write a pogram to concat a given string to lowercase
+
+// qus 5 => Extract the amount out of the string -> "The price is 1000 BDT"
+
+// qus 6 => Try to change the 4rth character of a given string. were you able to do it?
