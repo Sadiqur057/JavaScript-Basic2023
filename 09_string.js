9268f9c64838cd3e7548c627c13dcdeb686d0c7c
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 14:00:09 +0600

    commit

--- 09_string.js
+++ 09_string.js
@@ -5,7 +5,23 @@ let myFriend = 'Akash';
 console.log(myName);
 console.log(myName.length);
 
-// Template literals
+// Single quotation can inside double quotation 
+let place = "Cox's Bazar";
+console.log(place);
+
+// Double quotation can inside  single quotation 
+place = '"Saint martin" could be a better choice';
+console.log(place);
+
+//  Escape Sequence Characters
+place = "Cox\'s Bazar";
+console.log(place);
+
+// Template literals => With template literals, it is possible to use both single and double quotos inside a string
+let info = `"Tamim" is best friend of 'Shakib'`;
+console.log(info);
+
+// String interpolation => variable ccan be inserted directly inside the template literal
 let person1 = "Tamim";
 let person2 = "Shakib";
 // Tamim is a friend of Shakib
