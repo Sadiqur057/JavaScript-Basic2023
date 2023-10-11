ece008cee2ed6205a9c87590a86f490cdcccc751
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 19:08:26 +0600

    commit

--- 09_string_method.js
+++ 09_string_method.js
@@ -4,17 +4,24 @@
 let myName = "Sadiq";
 console.log(myName.length);
 
-// toUpperCase() & toLowerCase()
+// toUpperCase() & toLowerCase() => convert
 console.log(myName.toUpperCase());
 console.log(myName.toLowerCase());
 
-// Slice
+// Slice() => slice a string
 console.log(myName.slice(1,4)); 
 
-// replace
+// replace() => replace string with another
 let myDetails = "I am Sadiqur Rahman, studying CSE at East West University";
 console.log(myDetails.replace("CSE","SE"));
 
-// concat
+// concat() => merge two string
 let myFriend = "Akash";
 console.log(myName.concat(" is a friend of ",myFriend));
+
+// trim() => clear all the space at starting and ending
+let myBrother = "    sayfur    ";
+console.log(myBrother.trim());
+
+// indexOf() => find position of particular string
+console.log(myDetails.indexOf("CSE"));
