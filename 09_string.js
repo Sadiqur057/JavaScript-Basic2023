16c7f22574c95d73f96d2ca2b871a5f612a098cc
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 13:52:31 +0600

    commit

---
+++ 09_string.js
@@ -0,0 +1,13 @@
+// String => collection of character
+// String used to manipulate and save text.
+let myName = "Sadiq";
+let myFriend = 'Akash';
+console.log(myName);
+console.log(myName.length);
+
+// Template literals
+let person1 = "Tamim";
+let person2 = "Shakib";
+// Tamim is a friend of Shakib
+let sentence = `${person1} is a friend of ${person2}`;
+console.log(sentence);
