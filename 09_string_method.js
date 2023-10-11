1802c8ab8af9f1d3fe94a6d3a1a662518738caf5
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-11 19:00:54 +0600

    commit

---
+++ 09_string_method.js
@@ -0,0 +1,20 @@
+// String method
+
+// length
+let myName = "Sadiq";
+console.log(myName.length);
+
+// toUpperCase() & toLowerCase()
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+// Slice
+console.log(myName.slice(1,4)); 
+
+// replace
+let myDetails = "I am Sadiqur Rahman, studying CSE at East West University";
+console.log(myDetails.replace("CSE","SE"));
+
+// concat
+let myFriend = "Akash";
+console.log(myName.concat(" is a friend of ",myFriend));
