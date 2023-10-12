9053d61023bb677f26820d6bd72cc76cc48446b8
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-12 23:02:29 +0600

    commit

---
+++ 10-2_arrays_method.js
@@ -0,0 +1,10 @@
+// toString() => return string;
+let numbers = [1,2,3,4,5];
+let convertedString = numbers.toString();
+console.log(convertedString);
+console.log(typeof convertedString);
+
+// join = join operation, return string
+let joinedString = numbers.join("_");
+console.log(joinedString);
+console.log(typeof joinedString);
