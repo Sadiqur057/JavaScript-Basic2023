9d57aa63beabec0389d864e64b5e198669837bab
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-12 23:07:41 +0600

    commit

--- 10-2_arrays_method.js
+++ 10-2_arrays_method.js
@@ -1,10 +1,15 @@
 // toString() => return string;
-let numbers = [1,2,3,4,5];
+let numbers = [1,2,3,4,5,9,11];
 let convertedString = numbers.toString();
 console.log(convertedString);
 console.log(typeof convertedString);
 
-// join = join operation, return string
-let joinedString = numbers.join("_");
+// join = use to change format, return string
+let joinedString = numbers.join("and");
 console.log(joinedString);
 console.log(typeof joinedString);
+
+//  pop = remove last elements
+// we can check removed element
+let removedElements = numbers.pop();
+console.log(`New array = ${numbers} and removed element is ${removedElements}`);
