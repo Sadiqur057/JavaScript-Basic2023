9cd394e6b75e276a3161d99a00c646c28491df87
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-14 00:06:47 +0600

    commit

---
+++ 10-5_solve.js
@@ -0,0 +1,21 @@
+// // qus 1 => Create an array of numbers and take input from the user to add numbers to this array
+// let arr = [1,2,3];
+// // arr[arr.length+1]=parseInt(prompt("Enter a number :")) ;
+// let a = parseInt(prompt("Enter a number"));
+// arr.push(a);
+
+
+// // qus 2 => Keep adding numbers to the array in qus 1 until 0 is added to the array.
+
+// while(a!=0){
+//     a = parseInt(prompt("Enter a number"));
+//     arr.push(a);
+// }
+// console.log(arr);
+
+// qus 3 => filter for numbers divisible by 10 from a given array
+
+
+// qus 4 => Create an array of square of given numbers
+
+// qus 5 => use reduce to calculate factorial of a given number from an array of first n natural numbers ( n being the number whose factorial need to be calculated)
