970a8c2ccc00913c567407dba6a245df90cfafd3
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-14 00:15:09 +0600

    commit

--- 10-5_solve.js
+++ 10-5_solve.js
@@ -15,11 +15,9 @@
 
 // qus 3 => filter for numbers divisible by 10 from a given array
 
-// let arr2 = [10,23,20,30,13,50,100];
+let arr2 = [10,23,20,30,13,50,100];
 // let filterFunc = ((x)=>{
-//     if(x%10==0){
-//         return x;
-//     }
+//     return x%10==0;
 // })
 // let x = arr2.filter(filterFunc);
 // console.log(x);
@@ -27,11 +25,14 @@
 // or
 
 let x = arr2.filter((x)=>{
-    if(x%10==0){
-        return x;
-    }
+    return x%10==0;
 })
 console.log(x);
 // qus 4 => Create an array of square of given numbers
+
+let x2 = arr2.map((x)=>{
+    return x*x;
+})
+console.log(x2);
 
 // qus 5 => use reduce to calculate factorial of a given number from an array of first n natural numbers ( n being the number whose factorial need to be calculated)
