8f9a8d194863a10a1288afd0cbc00161566676f3
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-14 21:13:27 +0600

    commit

--- 11-1_exercise.js
+++ 11-1_exercise.js
@@ -1,7 +1,8 @@
 // Write a javascript program to generate a random number and store it in a variable. The program then takes an input from the user to tell them whether the guess is correct, greater or lower than the original number
 
 let guessingNum = prompt("Guess the number: ");
-let winningNum = 23;
+let winningNum = Math.floor(Math.random()*100);
+console.log("winningNum");
 while(guessingNum!=winningNum){
     winningNum = prompt("Incorrect! Guess again: ");
     if(guessingNum<winningNum&& winningNum>0){
