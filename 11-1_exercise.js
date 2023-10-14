2c4294218bd423b4b1b365cbe5e68e46e71ac469
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-14 20:57:06 +0600

    commit

---
+++ 11-1_exercise.js
@@ -0,0 +1,2 @@
+// Write a javascript program to generate a random number and store it in a variable. The program then takes an input from the user to tell them whether the guess is correct, greater or lower than the original number
+
