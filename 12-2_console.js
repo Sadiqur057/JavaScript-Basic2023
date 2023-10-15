94cab959426f51704fe59dfee01177339fba2783
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-15 21:45:13 +0600

    commit

---
+++ 12-2_console.js
@@ -0,0 +1,11 @@
+// console.log(console);
+console.assert(5<2);  // if false then assertion failed
+console.clear();
+let obj = {
+    a:1,
+    b:2,
+    c:3
+}
+console.table(obj);
+console.warn("This is a warn");
+console.info("This is a info");
