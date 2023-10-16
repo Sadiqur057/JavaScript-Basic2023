c1e492b19ef3129d22bf2b07a37fe0528fbf511b
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-16 22:30:14 +0600

    commit

--- 15-2_solve.js
+++ 15-2_solve.js
@@ -33,3 +33,17 @@ num = parseInt(num);
 if(num>4){
     location.href="https://google.com/"
 }
+
+// qus 5 => Change the background of the page to yellow, red, green based on user input through prompt
+let color = prompt("Chose background color: Enter a number(1-3) 1 = yellow , 2 = red, 3 = green ");
+color = parseInt(color);
+if(color==1){
+    document.body.style.background="yellow";
+}else if(color==1){
+    document.body.style.background="yellow";
+}else if(color==1){
+    document.body.style.background="yellow";
+}else{
+    alert("Please enter a number between 1-3 next time");
+}
+
