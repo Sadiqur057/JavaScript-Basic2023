ac8530ac310a3acb3887a710b9df468b32da3463
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-16 22:51:48 +0600

    commit

---
+++ 16-2_exercise.js
@@ -0,0 +1,1 @@
+// Use JavaScript to create a game of snake water and gun. The game should ask you to enter S, W or G. The computer should be able to randomly generate S, W or G and declare win or loss using alert. Use confirm and prompt whenever required.
