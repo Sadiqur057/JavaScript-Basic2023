38b5e66f693dd5fa860f9589f293590cb616c464
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-16 22:28:41 +0600

    commit

---
+++ 15-1_practice_qus.js
@@ -0,0 +1,9 @@
+// qus 1 => Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive based on age, age>= 18 can drive
+
+// qus 2 => In qus1 use Confirm to ask the user if he wants to see the prompt again
+
+// qus 3 => In the previous qus use console.error to log the error if the age entered is negative
+
+// qus 4 => Write a program to change the url to google.com(redirection) of user enters a number greater than 4
+
+// qus 5 => Change the background of the page to yellow, red, green based on user input through prompt
