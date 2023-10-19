e71c4a10f9b2c158e932388f239f7e7a92272023
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-19 23:38:11 +0600

    commit

---
+++ 20-1_practice_qus.js
@@ -0,0 +1,10 @@
+// qus 1 => Create anavbar and change the color of its first element to red
+
+// qus 2 => Create a table without tbody. Now use "view page source" button to check whether it has a tbody or not
+
+// qus 3 => Create an element with 3 children. Now change the color or first and last element to green
+
+// qus 4 => Write a javascript program to change background of all <li> tags to cyan
+
+// qus 5 => Which of the following is used to look for the farthest ancestor that matches a given CSS selector
+let options = ["matches","closest","contains","none of this above"];
