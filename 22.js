bfd05a001c1014c82d243a756d4d33189c6bdd91
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-21 20:28:02 +0600

    commit

---
+++ 22.js
@@ -0,0 +1,5 @@
+// console.log() =>  shows the element DOM tree
+
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+// console.dir() => shows the element as an object with its properties
