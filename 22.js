7d9d742385490511ed2fcf7efe8411316ba925cd
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-21 20:44:48 +0600

    commit

--- 22.js
+++ 22.js
@@ -1,5 +1,18 @@
+// tagName & nodeName => used to read tag name for element nodes
+console.log(document.body.firstChild.tagName); // will return undefined for text
+console.log(document.body.firstElementChild.tagName);
+// tagName = only exists for element nodes
+//nodeName = defined for array node (text, comment etc)
+console.log(document.body.firstElementChild.nodeName);
+console.log(document.body.firstChild.nodeName);
 // console.log() =>  shows the element DOM tree
 
+let x = document.getElementsByTagName("span")[0];
+console.log(x);
 console.log(document.body.firstChild.nodeName);
 console.log(document.body.firstElementChild.nodeName);
 // console.dir() => shows the element as an object with its properties
+let y = document.getElementsByTagName("span")[0];
+console.dir(y);
+console.dir(document.body.firstChild.nodeName);
+console.dir(document.body.firstElementChild.nodeName);
