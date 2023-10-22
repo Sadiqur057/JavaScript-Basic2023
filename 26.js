16406caa3e0e2c407681ec9c350d0c30df19039f
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-22 23:10:18 +0600

    commit

--- 26.js
+++ 26.js
@@ -1,4 +1,15 @@
-alert("Hello");
-setTimeout(function(){
+document.write("Hello");
+// setTimeout()
+let a = setTimeout(function(){
     alert("I am inside the setTimeout");
 },2000)
+
+// clearTimeout() => clear timeout
+clearTimeout(a);
+
+const sum = (a,b) =>{
+    console.log("running"+ (a+b));
+    a+b;
+}
+
+setTimeout(sum,1000,1,2);
