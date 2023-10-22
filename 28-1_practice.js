19f854606bf9f3ab08452afbc7126d1f7d4f2b73
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-22 23:52:15 +0600

    commit

---
+++ 28-1_practice.js
@@ -0,0 +1,9 @@
+// qus 1 => Write a program to show different alerts when different buttons are clicked
+
+// qus 2 => Create a website which is capable of storing bookmarks of your favourite website using href
+
+// qus 3 => Repeat qus 2 using event listeners
+
+// qus 4 => Write a javascript program to keep fetching contents of a website(every 5 second)
+
+// qus 5 => Create a glowing bulb effect using classList toggle method is js
