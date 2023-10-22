99a6abfff9c4516512521a64c1c9e45d69274daa
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-22 23:35:24 +0600

    commit

--- 27.js
+++ 27.js
@@ -1,7 +1,14 @@
 let btn = document.getElementById("btn");
-btn.addEventListener('click',function(e){
+let x = ()=>{
     alert("Hello world 1");
-})
-btn.addEventListener('click',function(e){
+}
+let y = ()=>{
     alert("Hello world 2");
-})
+}
+btn.addEventListener('click',x);
+btn.addEventListener('click',y);
+
+let a = parseInt(prompt("what is your fav number?"));
+if(a==2){
+    btn.removeEventListener('click',x);
+}
