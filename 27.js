7ecf2ecd465bf022a1f67c0dce45c485471c878c
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-22 23:43:05 +0600

    commit

--- 27.js
+++ 27.js
@@ -1,8 +1,11 @@
+// Always pass same reference function for add and remove
+// addEventListener is used to assign multiple handlers to an event
 let btn = document.getElementById("btn");
-let x = ()=>{
+let x = (e)=>{
     alert("Hello world 1");
+    // console.log(e.target.value);
 }
-let y = ()=>{
+let y = (e)=>{
     alert("Hello world 2");
 }
 btn.addEventListener('click',x);
@@ -12,3 +15,6 @@ let a = parseInt(prompt("what is your fav number?"));
 if(a==2){
     btn.removeEventListener('click',x);
 }
+
+// Event Object = when an event happens, the browser create an event object, puts details into it and passesit as an arguement to the handler
+
