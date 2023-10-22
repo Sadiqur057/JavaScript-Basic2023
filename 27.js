ff0f8f6b9233d748b427d08a4f652e0eed3f2bff
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-22 23:27:52 +0600

    commit

---
+++ 27.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn");
+btn.addEventListener('click',function(e){
+    alert("Hello world 1");
+})
+btn.addEventListener('click',function(e){
+    alert("Hello world 2");
+})
