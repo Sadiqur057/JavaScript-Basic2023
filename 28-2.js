d7492a2c23f2139fb446f3abb6ed96efe0fc44ab
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-23 13:17:17 +0600

    commit

--- 28-2.js
+++ 28-2.js
@@ -1,8 +1,32 @@
-// qus 1 => Write a program to show different alerts when different buttons are clicked
+// qus 1 => Write a program to show different alerts and change body color when different buttons are clicked
+let intro = document.getElementById("intro");
 let btn1 = document.getElementById("btn1");
 btn1.onclick = () =>{
-    alert("This is alert 1");
+    let x =confirm("Do you want to change background color to Green?");
+    if(x){
+        document.body.style.background="green";
+        intro.style.color = "white";
+    }
 } 
+let btn2 = document.getElementById("btn2");
+btn2.onclick = () =>{
+    let x =confirm("Do you want to change background color to Red?");
+    if(x){
+        document.body.style.background="red";
+        intro.style.color = "white";
+    }
+} 
+
+let btn3 = document.getElementById("btn3");
+btn3.onclick = () =>{
+    let x =confirm("Do you want to change background color to Blue?");
+    if(x){
+        document.body.style.background="blue";
+        intro.style.color = "white";
+    }
+} 
+
+ 
 // qus 2 => Create a website which is capable of storing bookmarks of your favourite website using href
 
 // qus 3 => Repeat qus 2 using event listeners
