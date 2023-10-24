6c192d374b7b249c1dcd510f870a4ac720551b54
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-24 18:40:33 +0600

    commit

--- 30-7_promise_api.js
+++ 30-7_promise_api.js
@@ -18,15 +18,22 @@ let p3 = new Promise((resolve,reject)=>{
 
 // Promise.all()
 // if one promise rejected then it will failed, no further promise will be executed
-let promise_all = Promise.all([p1,p2,p3]);
-promise_all.then((value)=>{
+// let promise_all = Promise.all([p1,p2,p3]);
+// promise_all.then((value)=>{
+//     console.log(value)
+// });
+
+// // Promise.allSettled()
+// // if one promise rejected then other promises will be executed
+// let promise_allSettled = Promise.allSettled([p1,p2,p3]);
+// promise_allSettled.then((value)=>{
+//     console.log(value)
+// });
+
+// Promise.race()
+// only show the fastest one
+// doesnot work on error
+let promise_race = Promise.race([p1,p2,p3]);
+promise_race.then((value)=>{
     console.log(value)
 });
-
-// Promise.all Settled()
-// if one promise rejected then other promises will be executed
-let promise_allSettled = Promise.allSettled([p1,p2,p3]);
-promise_allSettled.then((value)=>{
-    console.log(value)
-});
-
