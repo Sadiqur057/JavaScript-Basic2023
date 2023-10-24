0039231482abc7a0ce0eb84ba949825c7994514d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-24 20:26:23 +0600

    commit

--- 31-1_async_await.js
+++ 31-1_async_await.js
@@ -6,16 +6,34 @@ async function weather(){
     let weatherDhaka = new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve("27deg");
-        },1000);
+        },2000);
     })
     
     let weatherPabna = new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve("25deg");
-        },3000);
+        },5000);
     })
-    weatherDhaka.then(alert);
-    weatherPabna.then(alert);
+    // weatherDhaka.then(alert);
+    // weatherPabna.then(alert);
+    console.log("Fetching Dhaka weather. Please wait");
+    let wD = await(weatherDhaka);
+    console.log("Fetched Dhaka weather : "+wD)
+
+    console.log("Fetching Pabna weather. Please wait");
+    let wP = await(weatherPabna);
+    console.log("Fetched Pabna weather : "+wP)
+
+    return[wD,wP];
 }
-console.log("Welcome to weather control room")
-weather();
+
+const randFunc = async () =>{
+    console.log("This is a random function");
+}
+
+const runMain = async () =>{
+    console.log("Welcome to weather control room")
+    let a = await weather();
+    let b = await randFunc();
+}
+runMain();
