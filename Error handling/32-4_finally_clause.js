75431b8fbea3cdd9746c3814ee8c0c42e273542a
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-25 22:13:03 +0600

    commit

---
+++ Error handling/32-4_finally_clause.js
@@ -0,0 +1,8 @@
+try{
+    let a = 0;
+    console.log("Executed successfully")
+}catch(err){
+    console.log("Error occured")
+}finally{
+    console.log("finally clause executed even if any error occured");
+}
