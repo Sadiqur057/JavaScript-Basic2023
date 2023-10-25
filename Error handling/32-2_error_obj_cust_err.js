f68a6714e6a2e917bc255ab2cf17e0df563d5812
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-25 21:20:26 +0600

    commit

--- Error handling/32-2_error_obj_cust_err.js
+++ Error handling/32-2_error_obj_cust_err.js
@@ -7,3 +7,16 @@ try{
     console.log(error.name); // error name is Reference error
     console.log(error.message); // error message is abc not defined
 }
+
+
+// customer error
+try{
+    console.log(xyz);
+    throw new ReferenceError("please define xyz");
+}catch(error){
+    console.log(error);
+    console.log(error.stack); 
+    console.log(error.name); // error name is Reference error
+    console.log(error.message); // error message is abc not defined
+}
+
