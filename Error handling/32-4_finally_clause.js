9b7e458ffc819f98c5ff18062532de4a31c95c77
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-27 10:34:11 +0600

    commit

--- Error handling/32-4_finally_clause.js
+++ Error handling/32-4_finally_clause.js
@@ -5,4 +5,24 @@ try{
     console.log("Error occured")
 }finally{
     console.log("finally clause executed even if any error occured");
+    // close the file
+    // exit loop
+    // write to the log file
+
 }
+
+const myFunc =() =>{
+    try{
+        let a = 0;
+        console.log("Executed successfully [inside func]")
+        return;
+    }catch(err){
+        console.log("Error occured [inside func]")
+    }finally{
+        console.log("finally clause executed even if any error occured or return called in try block [inside func]");
+        
+    
+    }
+    
+}
+myFunc();
