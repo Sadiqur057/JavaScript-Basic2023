7d8f3c28dd526c49cf88a0121914dd0f87eb5e43
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-27 10:38:22 +0600

    commit

---
+++ Error handling/33-1_practice9.js
@@ -0,0 +1,7 @@
+//  write a program to load a javascript file in a browser using promises, Use .then() to display an alert when the load is complete
+
+// Write the same program from previous qus and use async/await syntax
+
+// Create a promise which rejects after 3 seconds. use an async/await to get its value. Use a try catch to handle its error
+
+// qus4 => Write a program using Promise.all() inside an async/ await to await 3 promises. Compare its result with the case where we await these promises one by one.
