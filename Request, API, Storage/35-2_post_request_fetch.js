27bd947d50a93ab7da8c4361c1c047aea46a5a9b
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-28 22:16:55 +0600

    commit

--- Request, API, Storage/35-2_post_request_fetch.js
+++ Request, API, Storage/35-2_post_request_fetch.js
@@ -19,13 +19,23 @@ const createToDO = async () => {
   return response;
 };
 
+const getTodo = async(id)=>{
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts/"+id);
+    let r = await response.json();
+    return r;
+}
+
 let todo = createToDO()
 .then((response)=>{
+    console.log(todo);
     console.log(response);
 })
-//and
+
+
 const mainFunc = async () => {
   let todo = await createToDO();
   console.log(todo);
+  let goto = await getTodo(11);
+  console.log(goto);
 };
 mainFunc();
