7c7f5b20e520813c325840fbb4f701e3fae44ff5
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-28 22:48:51 +0600

    commit

--- Request, API, Storage/36-1_cookies.js
+++ Request, API, Storage/36-1_cookies.js
@@ -1,6 +1,18 @@
 console.log(document.cookie);
+// document.cookie is a set call
 document.cookie= "name=sadiq";
 document.cookie= "age=23";
 document.cookie= "dept=cse";
 document.cookie= "dept=se"; // it will replace the last dept value
+let key = "$3536%";
+let value = "$3536%";
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+console.log(document.cookie);
+// to decode the component
+let x = "%243536%25"
+console.log(decodeURIComponent(x));
+
+// note
+// should not exceed 4kb
+// total number cookies around 20+/-
 
