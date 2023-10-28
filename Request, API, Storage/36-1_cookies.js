efb5242633bb543e319966de6a882e7af9419bce
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-28 22:34:15 +0600

    commit

---
+++ Request, API, Storage/36-1_cookies.js
@@ -0,0 +1,6 @@
+console.log(document.cookie);
+document.cookie= "name=sadiq";
+document.cookie= "age=23";
+document.cookie= "dept=cse";
+document.cookie= "dept=se"; // it will replace the last dept value
+
