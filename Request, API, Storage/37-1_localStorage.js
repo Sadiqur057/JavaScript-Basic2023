888e02ad86c9b6779858ad219d34a95f16aa4560
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-28 23:22:14 +0600

    commit

---
+++ Request, API, Storage/37-1_localStorage.js
@@ -0,0 +1,6 @@
+// local storag is a web storage which are not send to browser with each request
+
+let key = "name";
+let value = "sadiq";
+localStorage.setItem(key,value);
+console.log(`The key is ${key} and the value is ${value}`);
