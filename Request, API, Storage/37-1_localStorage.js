eefb3ea8c65e4715367bfcb8f64dd7cb85fa6634
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-28 23:43:21 +0600

    commit

--- Request, API, Storage/37-1_localStorage.js
+++ Request, API, Storage/37-1_localStorage.js
@@ -4,3 +4,18 @@ let key = "name";
 let value = "sadiq";
 localStorage.setItem(key,value);
 console.log(`The key is ${key} and the value is ${value}`);
+
+let key2 = "age";
+let value2 = 23;
+localStorage.setItem(key2,value2);
+console.log(`The key is ${key2} and the value is ${value2}`);
+
+console.log(localStorage.length);
+
+localStorage.removeItem("age");
+console.log(localStorage.length);
+
+console.log(localStorage.key(0));
+// keys and values stored as a string
+
+
