95743178bb6168df98f90bccb6a5ce6f4ddabdac
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-29 10:05:56 +0600

    commit

---
+++ Request, API, Storage/37-2_sessonStorage.js
@@ -0,0 +1,11 @@
+// Session storage => properties and method are almost same
+// work on only same tab
+// survive on tab refresh but end with closing tab
+
+
+sessionStorage.setItem("name","sadiq");
+sessionStorage.setItem("age","23");
+sessionStorage.setItem("dept","cse");
+sessionStorage.getItem("name");
+sessionStorage.clear();
+sessionStorage.removeItem("age");
