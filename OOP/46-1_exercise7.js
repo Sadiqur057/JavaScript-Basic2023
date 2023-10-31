44a858e09c82edd6cb773a4fefdd0921835c98a1
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-31 21:14:15 +0600

    password generator

---
+++ OOP/46-1_exercise7.js
@@ -0,0 +1,50 @@
+// Create a js program that capable of generating a password which contains atleast one lowercase, one uppercase and one special characters. Create a password class to achieve the same
+
+class PasswordGenerator{
+    constructor(){
+        console.log("welcome to password generator");
+        this.pass = "";
+    }
+    generatePassword(len,difficulties){
+        let alphabets = "abcdefghijklmnopqrstuvwxyz";
+        let numbers = "1234567890";
+        let specialChar = "!@@#$%^&*";
+
+        if(len<3){
+            console.log("Password should be atleast 3 character long");
+        }else if(difficulties==1){
+            let i =0;
+            while(i<len){
+                this.pass+=alphabets[Math.floor(Math.random()*len)];
+                this.pass+=numbers[Math.floor(Math.random()*len)];
+                i+=2;
+            }
+            this.pass = this.pass.substring(0,len)
+            return this.pass;
+        }
+        else if(difficulties==2){
+            let i =0;
+            while(i<len){
+                this.pass+=alphabets[Math.floor(Math.random()*len)];
+                this.pass+=numbers[Math.floor(Math.random()*len)];
+                this.pass+=specialChar[Math.floor(Math.random()*len)];
+                i+=3;
+            }
+            this.pass = this.pass.substring(0,len)
+            return this.pass;
+        }else if(difficulties==3){
+            let i =0;
+            while(i<len){
+                this.pass+=alphabets[Math.floor(Math.random()*len)];
+                this.pass+=alphabets[Math.floor(Math.random()*len)].toUpperCase();
+                this.pass+=numbers[Math.floor(Math.random()*len)];
+                this.pass+=specialChar[Math.floor(Math.random()*len)];
+                i+=3;
+            }
+            this.pass = this.pass.substring(0,len)
+            return this.pass;
+        }
+    }
+}
+let p1 = new PasswordGenerator();
+console.log(p1.generatePassword(7,3));
