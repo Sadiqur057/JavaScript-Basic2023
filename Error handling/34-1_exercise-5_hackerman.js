27782493319ba355e72408db09880dba069e91c1
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-31 10:24:59 +0600

    commit

--- Error handling/34-1_exercise-5_hackerman.js
+++ Error handling/34-1_exercise-5_hackerman.js
@@ -4,46 +4,81 @@
 // Hacking your username
 // username found abc13
 // conncecting to facebook
+
+let messages = [
+  "Initializing hack tool",
+  "Connecting to facebook",
+  "Connceting to server 1",
+  "Connection failed. Retrying",
+  "Connecting to server 2",
+  "Connected successfully..",
+  "Username found",
+  "Fetching password",
+  "Trying Bruteforce attack",
+  "password matched",
+  "Account is hacked"
+]
+
+const sleep = async (sec)=>{
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve(true);
+    }, sec*1000);
+  })
+}
 let a = document.getElementById("title");
+const showHack = async(msg)=>{
+  await sleep(2);
+  a.innerHTML +="<br>" +msg
+}
 
-let p1 = () => {
-    return new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    resolve(1);
-    a.innerHTML = "Initializing hack program";
-  }, 3000);
-  })
-};
-let p2 = () => {
-    return new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    resolve(1);
-    a.innerHTML = "Hacking your username";
-  }, 3000);
-  })
-};
-let p3 = () => {
-    return new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    resolve(1);
-    a.innerHTML = "Username found abc13";
-  }, 3000);
-  })
-};
-let p4 = () => {
-    return new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    resolve(1);
-    a.innerHTML = "Connecting to facebook";
-  }, 3000);
-  })
-};
+(async()=>{
 
-let runHack = async () => {
-    let x1 = await p1();
-    let x2 = await p2();
-    let x3 = await p3();
-    let x4 = await p4();
-};
-runHack();
-// try to use HTML & styling if possible
+  for(i=0;i<messages.length;i++){
+    await showHack(messages[i]);
+  }
+
+})()
+
+
+// let p1 = () => {
+//     return new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     resolve(1);
+//     a.innerHTML = "Initializing hack program";
+//   }, 3000);
+//   })
+// };
+// let p2 = () => {
+//     return new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     resolve(1);
+//     a.innerHTML = "Hacking your username";
+//   }, 3000);
+//   })
+// };
+// let p3 = () => {
+//     return new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     resolve(1);
+//     a.innerHTML = "Username found abc13";
+//   }, 3000);
+//   })
+// };
+// let p4 = () => {
+//     return new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     resolve(1);
+//     a.innerHTML = "Connecting to facebook";
+//   }, 3000);
+//   })
+// };
+
+// let runHack = async () => {
+//     let x1 = await p1();
+//     let x2 = await p2();
+//     let x3 = await p3();
+//     let x4 = await p4();
+// };
+// runHack();
+// // try to use HTML & styling if possible
