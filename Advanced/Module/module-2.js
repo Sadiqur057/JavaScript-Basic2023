9d506dc9570f82132855426077fb6c489b9e84d0
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-02 16:30:58 +0600

    ES6 modules used

---
+++ Advanced/Module/module-2.js
@@ -0,0 +1,12 @@
+// ES6 modules
+export const hello = (name)=>{
+    console.log("Hello "+name);
+}
+export const greet = ()=>{
+    console.log("Good morning");
+}
+
+const welcome = ()=>{
+    console.log("Welcome!");
+}
+export default welcome;
