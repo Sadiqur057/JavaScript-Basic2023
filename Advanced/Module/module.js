a50f0d532e81a1f67a9e51b226767be36d0cdb83
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-02 16:31:33 +0600

    ES6 module introduced

--- Advanced/Module/module.js
+++ Advanced/Module/module.js
@@ -1,3 +1,14 @@
-const msg = require("./module-1");
-msg.hello("Sadiq");
-msg.greet();
+// const msg = require("./module-1");
+// msg.hello("Sadiq");
+// msg.greet();
+
+// common js modules => using destructuring 
+// const {hello,greet} = require("./module-1"); // commonjs
+// hello("akash");
+// greet();
+
+// ES6 Modules
+import welcome,{hello,greet} from "./module-2.js";
+hello("akash");
+greet();
+welcome();
