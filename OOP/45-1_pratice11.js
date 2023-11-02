6620f37ed6d850e9bad5a4ce410391f0ced33e2e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-02 20:07:12 +0600

    practiceSet-11

---
+++ OOP/45-1_pratice11.js
@@ -0,0 +1,9 @@
+// qus1 = > Create a javascript class to create a complex number. Create a constructor to set the real and complext part
+
+// qus2 => Write a method to add the complex numbers in the class
+
+// qus3 => Create a class Student from a class Human. Override a method and see changes
+
+// qus4=> See if student is an instance of Human using instanceof keyword
+
+// qus 5 -> Use getter and setter and get the real and imaginary parts of the complex number
