7448784625cc1ab60f2addf092df258f14bc7c59
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-05 22:54:07 +0600

    desctructured

--- Advanced/48_desctructuring.js
+++ Advanced/48_desctructuring.js
@@ -15,7 +15,7 @@ console.log(x,y);
 // Spread syntax => Allows an iterable such as an array on string to b expanded in places where zero or more arguments are expected. In an object literal, the spread syntax enumerate the properties of an object and adds the key-value pairs to the object being created
 let arr2 = [2,7,5];
 let obj2 = {...arr2};
-console.log(obj2);
+console.log(obj2); // {0:2,1:7,2:5}
 
 const sum = (v1,v2,v3)=>{
     return v1+v2+v3;
